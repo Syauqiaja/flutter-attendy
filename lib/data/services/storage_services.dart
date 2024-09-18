6b3c4a65d3f794f15faf3db9da4bacd7d362c2de
Ahmad Fajruddin Syauqi <64742395+Syauqiaja@users.noreply.github.com>
2024-09-18 15:05:11 +0700

    Fixed some mistakes

--- lib/data/services/storage_services.dart
+++ lib/data/services/storage_services.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:attendy_flutter/core/utils/keys.dart';
+import 'package:attendy_flutter/data/models/place.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class StorageService extends GetxService {
   late GetStorage _box;
@@ -10,9 +13,19 @@ class StorageService extends GetxService {
     _box = GetStorage();
     _box.listen(() => print("StorageService: GetStorage changed"));
     _box.listenKey(attendanceKey, (val) => print("Changes: $val"));
+
+    Place firstPlace = const Place(
+      latLng: LatLng(10, -100),
+      id: 1,
+      title: "Michigan Auss",
+      fullAddress: "It is the full address",
+      checkInTime: "08:00 AM",
+      checkOutTime: "05:00 PM",
+    );
+
     await _box.writeIfNull(attendanceKey, []);
-    await _box.writeIfNull(placeKey, []);
-    await _box.writeIfNull(selectedPlace, []);
+    await _box.writeIfNull(placeKey, jsonEncode([firstPlace]));
+    await _box.writeIfNull(selectedPlace, jsonEncode([firstPlace]));
     return this;
   }
 
